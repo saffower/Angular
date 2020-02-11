fe13fd4722e8a9e00f3bb5fd1ee9e91f3d60985f
Dan Deng <huidh@yahoo.com>
2020-02-11 15:02:55 -0500

    'message'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {StockInterface} from './services/stock-interface';
+import {StocksService} from './services/stocks.service';
+
+//let symbols: Array<string>=['AAPL', 'GOOG', 'FB', 'AMZN', 'TWTR' ];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'stocks';
+  // stocks: Array<StockInterface>;
+  // symbols: Array<string>;
+  // constructor(private service: StocksService){
+    //  this.service.load(['APPL']).subscribe(stocks=>{
+    //    this.stocks=stocks;
+    //  }
+      // );
+   //}
+
+  //  ngOnInit(){
+  //    this.service.load(this.symbols).subscribe(stocks=>{
+  //      this.stocks=stocks;
+  //    });
+  //  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {StocksService} from './services/stocks.service';
+import {SummaryComponent} from './components/summary/summary.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {ManageComponent} from './components/manage/manage.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SummaryComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
